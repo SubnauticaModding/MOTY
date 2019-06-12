10ed26efdd2cb2199045147358bc953401a35589
Glitch (moty) <none>
2019-06-12 19:21:29 +0000

    🍬🛢 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,45 @@
+// server.js
+// where your node app starts
+
+// init project
+const request = require('request');
+const http = require("http");
+const https = require("https");
+const express = require('express');
+const app = express();
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+
+
+app.get('*', async (req, res) => {
+  if (req.path.match(/^\/cs\.gif$/i)) {
+    res.redirect('https://cdn.glitch.com/578b3caa-2796-42d7-9bcb-bf1b681e8670%2FSNModding.gif');
+  } else if (req.path.match(/^\/alterra\.png$/i)) {
+    res.redirect('https://cdn.glitch.com/578b3caa-2796-42d7-9bcb-bf1b681e8670%2FAlterraLogo.png');
+  } else if (req.path.match(/^\/loadtest$/i)) {
+    res.sendFile(__dirname + '/loading_test.html');
+  } else {
+    res.sendFile(__dirname + "/coming_soon.html");
+  }
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Webserver started. Port: ' + listener.address().port);
+});
+
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+  setInterval( function()  {
+    bot.user.setStatus('idle');
+    bot.user.setActivity('games until December 1st');
+  }, 1000);
+});
+
+bot.on('message', msg => {
+  
+});
+
+
+bot.login(process.env.DISCORD_TOKEN);
