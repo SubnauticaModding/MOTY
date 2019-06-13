c8ccba4b2b70747f1160136348beb52e67b32185
AlexejheroYTB <alexejhero@gmail.com>
2019-06-13 12:55:03 +0300

    .

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const https = require("https");
 const express = require('express');
 const app = express();
 const Discord = require('discord.js');
+const util = require('./util.js');
+const config = require("./config.json");
 
 const bot = new Discord.Client();
 
+await util.enableLoggingProxy(bot);
 
 app.get('*', async (req, res) => {
   if (req.path.match(/^\/cs\.gif$/i)) {
@@ -27,34 +30,47 @@ app.post('*', async (req, res) => {
   if (req.path.match(/^\/gitsecret8712431974365292354262452948525742562454352654$/i)) {
     if (req.headers['x-github-event'] == "push") {
       cmd.run('chmod 777 git.sh'); /* :/ Fix no perms after updating */
-      cmd.get('./git.sh', (err, data) => {  // Run our script
+      cmd.get('./git.sh', (err, data) => { // Run our script
         if (data) console.log(data);
         if (err) console.log(err);
       });
-      cmd.run('refresh');  // Refresh project
+      cmd.run('refresh'); // Refresh project
       console.log("> [GIT] Updated with origin/master");
     }
     return res.sendStatus(200);
   }
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Webserver started. Port: ' + listener.address().port);
-
 });
 
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
-  setInterval( function()  {
+  setInterval(function () {
     bot.user.setStatus('idle');
     bot.user.setActivity('games until December 1st');
   }, 1000);
 });
 
-bot.on('message', msg => {
-  
-});
+async function getModInfo(game, id) {
+  var response = await fetch("http://api.nexusmods.com/v1/games/" + game + "/mods/" + id + ".json", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "apikey": process.env.NEXUS_TOKEN
+    }
+  });
+  var object = await response.json();
 
+  if (Number.parseInt(response.headers["x-rl-daily-remaining"]) == 0)
+    console.error("API KEY HAS NO MORE REQUESTS AVAILABLE!");
+
+  var result = {};
+  result.name = object.name;
+  result.image = object.picture_url;
+
+  return result;
+}
 
 //bot.login(process.env.DISCORD_TOKEN);
---
+++ util.js
@@ -0,0 +1,62 @@
+const config = require("./config.json");
+
+// Adds a proxy to console methods to send messages in chat
+module.exports.enableLoggingProxy = async (client) => {
+  const debug = console.debug;
+  const log = console.log;
+  const info = console.info;
+  const warn = console.warn;
+  const error = console.error;
+  const exception = console.exception;
+
+  console.debug = (info_, params) => {
+    client.emit('debug', info_);
+    if (params) debug(info_, params);
+    else debug(info_);
+  };
+  console.log = (info_, params) => {
+    client.emit('info', info_);
+    if (params) log(info_, params);
+    else log(info_);
+  };
+  console.info = (info_, params) => {
+    client.emit('info', info_);
+    if (params) info(info_, params);
+    else info(info_);
+  };
+  console.warn = (info_, params) => {
+    client.emit('warn', info_);
+    if (params) warn(info_, params);
+    else warn(info_);
+  };
+  console.error = (info_, params) => {
+    client.emit('error', info_);
+    if (params) error(info_, params);
+    else error(info_);
+  };
+  console.exception = (info_, params) => {
+    client.emit('error', info_);
+    exception(info_, params);
+  };
+
+  client.debug = message => console.debug(message);
+  client.log = message => console.log(message);
+  client.info = message => console.info(message);
+  client.warn = message => console.warn(message);
+  client.error = message => console.error(message);
+  client.exception = message => console.error(message);
+
+  client.on('debug', (s) => {
+    if (s.includes('[connection]')) return;
+    client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Debug').setDescription(s));
+  });
+  client.on('info', (s) => {
+    client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Log').setDescription(s).setColor('BLUE'));
+  });
+  client.on('warn', (s) => {
+    client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Warn').setDescription(s).setColor('ORANGE'));
+  });
+  client.on('error', (s) => {
+    client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Error').setDescription(s).setColor('RED'));
+  });
+};
