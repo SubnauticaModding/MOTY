c92e28dd3464e7912c4ad7733653940e50364c1a
Glitch (subnauticamodding-moty) <none>
2019-10-19 19:56:30 +0000

    Glitch Update

---
+++ src/unvote.js
@@ -0,0 +1,24 @@
+const auth = require('../src/auth');
+const authors = require('../src/authors');
+const server = require('../server');
+const users = require('../src/users');
+
+module.exports = function (data) {
+  try {
+    if (auth.sessionValid(data.authUserID, data.authSession)) return data.res.sendStatus(401);
+    if (!data.req.query.id) return data.res.sendStatus(400);
+    
+    var modautors = authors.getAuthors().map(a => a.id);
+    if (!modautors.includes(data.req.query.id)) return data.res.sendStatus(404);
+    
+    var user = users.getUser(data.user.user.id);
+    if (!user.votes.includes(data.req.query.id)) return data.res.sendStatus(304);
+    
+    user.votes = user.votes.filter(v => v != data.req.query.id);
+    users.setUserObject(user);
+    data.res.sendStatus(200);
+  } catch (e) {
+    console.error(e);
+    data.res.sendStatus(500);
+  }
+}
