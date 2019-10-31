ed45906997837d3bc8d5dbcde93ba0954f4987cf
Glitch (sn-moty) <none>
2019-10-31 12:40:13 +0000

    Rewound to commit b9c0c3cd402cc3db98ba1c33b77665ea4c4798f5 + 0 edits

--- src/modcache.js
+++ src/modcache.js
@@ -3,61 +3,43 @@ const mods = require("./mods");
 const request = require("request-promise");
 const server = require("../server");
 
-module.exports.update = async function() {
-  var snupdates = await request(
-    "https://api.nexusmods.com/v1/games/subnautica/mods/updated.json?period=1d",
-    {
-      headers: {
-        apikey: process.env.NEXUS_TOKEN
-      },
-      resolveWithFullResponse: true
-    }
-  );
-  console.log(
-    snupdates.headers["x-rl-daily-remaining"] + " requests remaining"
-  );
-  if (snupdates.headers["x-rl-daily-remaining"] % 100 == 0)
-    sendUpdate("daily", snupdates.headers["x-rl-daily-remaining"]);
+module.exports.update = async function () {
+  var snupdates = await request("https://api.nexusmods.com/v1/games/subnautica/mods/updated.json?period=1d", {
+    headers: {
+      "apikey": process.env.NEXUS_TOKEN,
+    },
+    resolveWithFullResponse: true,
+  });
+  console.log(snupdates.headers["x-rl-daily-remaining"] + " requests remaining");
+  if (snupdates.headers["x-rl-daily-remaining"] % 100 == 0) sendUpdate("daily", snupdates.headers["x-rl-daily-remaining"]);
   snupdates = JSON.parse(snupdates.body);
-  var bzupdates = await request(
-    "https://api.nexusmods.com/v1/games/subnauticabelowzero/mods/updated.json?period=1d",
-    {
-      headers: {
-        apikey: process.env.NEXUS_TOKEN
-      },
-      resolveWithFullResponse: true
-    }
-  );
-  console.log(
-    bzupdates.headers["x-rl-daily-remaining"] + " requests remaining"
-  );
-  if (bzupdates.headers["x-rl-daily-remaining"] % 100 == 0)
-    sendUpdate("daily", bzupdates.headers["x-rl-daily-remaining"]);
+  var bzupdates = await request("https://api.nexusmods.com/v1/games/subnauticabelowzero/mods/updated.json?period=1d", {
+    headers: {
+      "apikey": process.env.NEXUS_TOKEN,
+    },
+    resolveWithFullResponse: true,
+  });
+  console.log(bzupdates.headers["x-rl-daily-remaining"] + " requests remaining");
+  if (bzupdates.headers["x-rl-daily-remaining"] % 100 == 0) sendUpdate("daily", bzupdates.headers["x-rl-daily-remaining"]);
   bzupdates = JSON.parse(bzupdates.body);
   await updateGame(snupdates, "subnautica");
   await updateGame(bzupdates, "subnauticabelowzero");
-};
+}
 
-module.exports.getAllCached = function() {
+module.exports.getAllCached = function () {
   return data.getObjects("cache");
-};
+}
 
-module.exports.cacheAll = async function() {
+module.exports.cacheAll = async function () {
   for (var mod of mods.getMods()) {
-    var modinfo = await request(
-      `https://api.nexusmods.com/v1/games/${mod.domain}/mods/${mod.nexusid}.json`,
-      {
-        headers: {
-          apikey: process.env.NEXUS_TOKEN
-        },
-        resolveWithFullResponse: true
-      }
-    );
-    console.log(
-      modinfo.headers["x-rl-daily-remaining"] + " requests remaining"
-    );
-    if (modinfo.headers["x-rl-daily-remaining"] % 100 == 0)
-      sendUpdate("daily", modinfo.headers["x-rl-daily-remaining"]);
+    var modinfo = await request(`https://api.nexusmods.com/v1/games/${mod.domain}/mods/${mod.nexusid}.json`, {
+      headers: {
+        "apikey": process.env.NEXUS_TOKEN,
+      },
+      resolveWithFullResponse: true,
+    });
+    console.log(modinfo.headers["x-rl-daily-remaining"] + " requests remaining");
+    if (modinfo.headers["x-rl-daily-remaining"] % 100 == 0) sendUpdate("daily", modinfo.headers["x-rl-daily-remaining"]);
     modinfo = JSON.parse(modinfo.body);
 
     saveInCache(mod.domain, mod.nexusid, {
@@ -68,7 +50,7 @@ module.exports.cacheAll = async function() {
       image: modinfo.picture_url
     });
   }
-};
+}
 
 function getFromCache(domain, modid) {
   return data.getObject("cache", domain + "_" + modid);
@@ -80,18 +62,14 @@ async function updateGame(updates, domain) {
     if (!mappedmods.includes(domain + "_" + update.mod_id)) continue;
 
     if (needsRecache(domain, update.mod_id, update.latest_mod_activity)) {
-      var mod = await request(
-        `https://api.nexusmods.com/v1/games/${domain}/mods/${update.mod_id}.json`,
-        {
-          headers: {
-            apikey: process.env.NEXUS_TOKEN
-          },
-          resolveWithFullResponse: true
-        }
-      );
+      var mod = await request(`https://api.nexusmods.com/v1/games/${domain}/mods/${update.mod_id}.json`, {
+        headers: {
+          "apikey": process.env.NEXUS_TOKEN,
+        },
+        resolveWithFullResponse: true,
+      });
       console.log(mod.headers["x-rl-daily-remaining"] + " requests remaining");
-      if (mod.headers["x-rl-daily-remaining"] % 100 == 0)
-        sendUpdate("daily", mod.headers["x-rl-daily-remaining"]);
+      if (mod.headers["x-rl-daily-remaining"] % 100 == 0) sendUpdate("daily", mod.headers["x-rl-daily-remaining"]);
       mod = JSON.parse(mod.body);
 
       saveInCache(domain, update.mod_id, {
@@ -120,9 +98,5 @@ function needsRecache(domain, modid, updatedate) {
 function sendUpdate(type, number) {
   var extra = "";
   if (number <= 500) extra = "<@183249892712513536> ";
-  server.bot.channels
-    .get("567009737422536903")
-    .send(
-      extra + "API key has " + number + " " + type + " requests remaining."
-    );
+  server.bot.channels.get("567009737422536903").send(extra + "API key has " + number + " " + type + " requests remaining.");
 }
