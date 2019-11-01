9020cd50e873a1a26680cdc50fcccb023e87d71b
Glitch (sn-moty) <none>
2019-11-01 11:55:26 +0000

    🛀👗 Checkpoint ./www/html/main.ejs:3491121/99 ./www/html/partials/header.ejs:3491121/16 ./api/login.js:3491121/208

--- api/login.js
+++ api/login.js
@@ -2,7 +2,10 @@ const auth = require("../src/auth");
 
 module.exports = function (data) {
   try {
-    if (auth.sessionValid(data.authUserID, data.authSession)) data.res.redirect("/");
+    if (auth.sessionValid(data.authUserID, data.authSession)) { 
+      if (data.user)
+      data.res.redirect("/");
+    }
     else data.res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=577052606694948885&redirect_uri=https%3A%2F%2Fsn-moty.glitch.me%2Fauth&response_type=code&scope=identify`);
   } catch (e) {
     console.error(e);
