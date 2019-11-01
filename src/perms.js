c0559dd4a6eab0a2af0f786ae3b2766992a2ccf5
Glitch (sn-moty) <none>
2019-11-01 00:00:36 +0000

    Glitch Update

--- src/perms.js
+++ src/perms.js
@@ -3,6 +3,6 @@ const server = require("../server");
 module.exports.isAdmin = function (user) {
   if (!user) return false;
   var memberInOurGuild = server.bot.guilds.get("324207629784186882").member(user);
-  if (memberInOurGuild && memberInOurGuild.hasPermission("BAN_MEMBERS")) return true;
+  if (memberInOurGuild && memberInOurGuild.hasPermission("KICK_MEMBERS")) return true;
   else return false;
 }
