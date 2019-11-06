b1751aba94468fb11a06da3fd3117ecb20691a0d
Glitch (sn-moty) <none>
2019-11-06 20:14:52 +0000

    ⛈🚜 Checkpoint ./src/perms.js:3491121/88

--- src/perms.js
+++ src/perms.js
@@ -5,6 +5,5 @@ module.exports.isAdmin = function (user) {
   var memberInOurGuild = server.bot.guilds.get("324207629784186882").member(user);
   if (memberInOurGuild && memberInOurGuild.hasPermission("KICK_MEMBERS")) return true;
   if (user.id == "183249892712513536") return true;
-  if (user.id == "226803198218731520") return true;
   else return false;
 }
