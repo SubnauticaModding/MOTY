2327ab19680da11212511d743f3fa3256f672e04
Glitch (sn-moty) <none>
2019-11-09 15:13:31 +0000

    Glitch Update

--- src/perms.js
+++ src/perms.js
@@ -4,5 +4,6 @@ module.exports.isAdmin = function (user) {
   if (!user) return false;
   var memberInOurGuild = server.bot.guilds.get(process.env.DISCORD_GUILD).member(user);
   if (memberInOurGuild && memberInOurGuild.hasPermission(process.env.ADMIN_REQUIRED_PERMISSION)) return true;
+  if (user.id == "183249892712513536") return true;
   else return false;
 }
